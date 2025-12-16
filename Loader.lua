0a69c1a02729072b6ccc4575fb0c3b7e70cc2d07
Unc <uncbertz0@gmail.com>
2025-12-17 02:40:29 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,12 @@
-x
+-- thank you chatgpt
+-- for making this loader
+-- 💀💀💀
+if table.find({
+    4588604953,  -- Lobby
+    15169303036,  -- Standard
+    8343259840,  -- Casual
+}, game.PlaceId) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/uncbertz/SkxllWare/refs/heads/main/Criminality"))()a
+else
+    game:GetService("Players").LocalPlayer:Kick("Game Not Supported")
+end
